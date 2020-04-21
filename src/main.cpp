92989d2bd6e1a2d2374403e2f870f22f8e64a3a5
Chris Gregory <czipperz@gmail.com>
2020-04-21 01:08:23 -0400

    Refactor

--- src/main.cpp
+++ src/main.cpp
@@ -20,18 +20,14 @@ static void add_value(cz::Vector<cz::Vector<cz::Str> >& buckets, cz::Str value) 
     buckets[value.len].push(value);
 }
 
-int main(int argc, char** argv) {
-    cz::Buffer_Array buffer_array;
-    buffer_array.create();
-    CZ_DEFER(buffer_array.drop());
-
-    cz::Vector<cz::Vector<cz::Str> > buckets = {};
-    CZ_DEFER(buckets.drop(cz::heap_allocator()));
-
+static void read_file(cz::Buffer_Array& buffer_array,
+                      cz::Vector<cz::Vector<cz::Str> >& buckets,
+                      FILE* file,
+                      char separator) {
     char buffer[1024];
     cz::String value = {};
     while (1) {
-        size_t buffer_len = fread(buffer, 1, sizeof(buffer), stdin);
+        size_t buffer_len = fread(buffer, 1, sizeof(buffer), file);
         if (buffer_len == 0) {
             add_value(buckets, value);
             break;
@@ -40,10 +36,10 @@ int main(int argc, char** argv) {
         const char* start = buffer;
         while (1) {
             cz::Str str = {start, buffer_len - (start - buffer)};
-            const char* end = str.find('\n');
+            const char* end = str.find(separator);
             if (end) {
                 value.reserve(buffer_array.allocator(), end - start);
-                value.append({start, end - start});
+                value.append({start, (size_t)(end - start)});
                 value.realloc(buffer_array.allocator());
 
                 add_value(buckets, value);
@@ -57,14 +53,28 @@ int main(int argc, char** argv) {
             }
         }
     }
+}
 
+static void print_results(cz::Vector<cz::Vector<cz::Str> >& buckets, FILE* file, char separator) {
     for (size_t i = 0; i < buckets.len(); ++i) {
         cz::Slice<cz::Str> bucket = buckets[i];
         std::sort(bucket.elems, bucket.elems + bucket.len);
         for (size_t j = 0; j < bucket.len; ++j) {
             cz::Str v = bucket[j];
-            fwrite(v.buffer, 1, v.len, stdout);
-            putchar('\n');
+            fwrite(v.buffer, 1, v.len, file);
+            putchar(separator);
         }
     }
+}
+
+int main(int argc, char** argv) {
+    cz::Buffer_Array buffer_array;
+    buffer_array.create();
+    CZ_DEFER(buffer_array.drop());
+
+    cz::Vector<cz::Vector<cz::Str> > buckets = {};
+    CZ_DEFER(buckets.drop(cz::heap_allocator()));
+
+    read_file(buffer_array, buckets, stdin, '\n');
+    print_results(buckets, stdout, '\n');
 }
